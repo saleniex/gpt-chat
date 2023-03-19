cd12272a045bf14d4de069de8c1f2051686fdd15
Uldis Jansons <uldisj@intranet.smc.lv>
2023-03-19 17:44:27 +0200

    feat: Implement HTTP POST in meta service

--- internal/meta/service.go
+++ internal/meta/service.go
@@ -1,6 +1,11 @@
 package meta
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type Service struct {
 	FromPhoneNumberId string
@@ -16,7 +21,15 @@ func (s Service) SendRequest(request ServiceRequest) error {
 	if err != nil {
 		return err
 	}
+	url := fmt.Sprintf("https://graph.facebook.com/v16.0/%s/messages", s.FromPhoneNumberId)
 	log.Printf("Send mesage: %s", dataStr)
-	//TODO perform actual sending
+
+	response, postErr := http.Post(url, "application/json", strings.NewReader(dataStr))
+	if postErr != nil {
+		return postErr
+	}
+	if response.StatusCode < 200 && response.StatusCode > 299 {
+		return fmt.Errorf("server responded with non-success status code %d", response.StatusCode)
+	}
 	return nil
 }
